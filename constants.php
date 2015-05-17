9eb3e79b1535e9dc450ee1b77d718b57248e5b9f
gmaksimov <maksimov.grisha@gmail.com>
2015-05-17 12:57:28 +0300

    buf fix
    Current version is working

--- constants.php
+++ constants.php
@@ -2,6 +2,7 @@
 $file = ($_SERVER['DOCUMENT_ROOT']."/../constants.txt");
 $f = file($file);
 for($i = 0; $i < count($f); $i++){
+	$f[$i] = str_replace("\n", "", $f[$i]);
 	$var = explode("=", $f[$i]);
 	global ${$var[0]};
 	${$var[0]} = $var[1];
